58237c24a2acc5531e82ed9af2934b30c2daf06c
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-02-23 10:51:23 +0200

    Add files via upload

---
+++ S-nastik/Sõnastik/Sõnastik/Sõnastik.py
@@ -0,0 +1,23 @@
+﻿from omaModul import *
+MasVene=loe_failist("vene.txt")
+MasEst=loe_failist("est.txt")
+while True:
+    v=int(input("1-tõlk:vene-eesti\n2-tõlk:eesti-vene\n3-lisa_sõnu\n4-Muuta\n5-TEST"))
+    if v==1:
+        laused=tõlk(MasVene,MasEst)
+        print(laused)
+    elif v==2:
+        laused=tõlk(MasEst,MasVene)
+        print(laused)
+    elif v==3:
+        laused=lisa_sõnu(MasEst,MasVene)
+        print(MasEst)
+        print(MasVene)
+    elif v==4:
+        laused=muutaSõnu(MasEst,MasVene)
+        print(MasEst)
+        print(MasVene)
+    elif v==5:
+        laused=test(MasEst,MasVene)
+        print(MasEst)
+        print(MasVene)
---
+++ S-nastik/Sõnastik/Sõnastik/omaModul.py
@@ -0,0 +1,68 @@
+﻿from random import *
+def loe_failist(f):
+    fail=open(f,"r",encoding="utf-8-sig")
+    mas=[]
+    for rida in fail:
+        mas.append(rida.strip())
+    fail.close()
+    return mas
+def tõlk(f:str,s:str):
+    x=input("Sise sõnu: ")
+    if x in f:
+        asv=f.index(x)
+        asd=s[asv]
+    else:
+        asd="Vale sõnu"
+    return(asd)
+def lisa_sõnu(f:str,s:str):
+    x=input("Esti sõna: ")
+    y=input("Vene sõna: ")
+    asv=f.append(x)
+    asd=s.append(y)
+    return(asv,asd)
+def muutaSõnu(f:list,s:list)->list:
+    xy=input("millist sõnastikku soovite parandada? Est või Vene")
+    if xy=="Est":
+        x=int(input("Nimetage sõna asukoht: "))
+        xyz=input("Sisse: ")
+        f.pop(x)
+        fa=f.insert(x,xyz)
+        f=open("est.txt", "a")
+        f.writelines(fa)
+        f.close()
+    elif xy=="Vene":
+        y=int(input("Nimetage sõna asukoht: "))
+        xyz=input("Sisse: ")
+        sa=f.insert(y,xyz)
+        s=open("vene.txt", "a")
+        s.writelines(sa)
+        s.сlose()
+    return f,s
+def test(f:str, s:str):
+    x=input("millist sõnastikku soovite test? Est või Vene")
+    vastusVale=0
+    vastusÕige=0
+    zxz=int(input("mitu katset soovite teha: "))
+    for i in range(zxz):
+        if x=="Est":
+            randomSõnu=randint(0,4)
+            print(f[randomSõnu])
+            zx=f.index(f[randomSõnu])
+            xy=input("Sisse: ")
+            if xy==s[zx]:
+                vastusÕige+=1
+            else:
+                vastusVale+=1
+        elif x=="Vene":
+            randomSõnu=randint(0,4)
+            print(s[randomSõnu])
+            zx=s.index(s[randomSõnu])
+            xy=input("Sisse: ")
+            if xy==f[zx]:
+                vastusÕige+=1
+            else:
+                vastusVale+=1
+    print("Õige",round(vastusÕige/zxz*100,1),"%")
+    print("Vale",round(vastusVale/zxz*100,1),"%")
+    f.write
+    return f, s
